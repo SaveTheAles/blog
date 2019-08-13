745c31141ca6c320eb21318353382fdb919b1a55
Ales Puchilo <ales.puchilo@gmail.com>
2019-08-13 14:14:24 +0300

    add ci skipping

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -7,7 +7,7 @@ git config user.name "Ales Puchilo"
 
 # echo "blog.seangransee.com" > CNAME # domain your site will live at
 git add .
-git commit -m "Deploy from CircleCI" # this will always be the only commit in your gh-pages branch
+git commit -m "Deploy from CircleCI [skip CI]" # this will always be the only commit in your gh-pages branch
 
 # use --quiet to avoid printing token in build logs
 git push --force --quiet https://${BLOG_GITHUB_TOKEN}@github.com/savetheales/blog.git master:gh-pages # path to your repo on GitHub, using token for authentication
