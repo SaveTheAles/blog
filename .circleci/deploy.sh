7deb43f13859b30ee18f59a0984ec1156e912dd0
Ales Puchilo <ales.puchilo@gmail.com>
2019-08-13 11:46:06 +0300

    rewrite ci and dd deploy script

---
+++ .circleci/deploy.sh
@@ -0,0 +1,13 @@
+hugo -v # command to build your site
+cd public # cd into folder containing your built site
+
+git init
+git config user.email "ales.puchilo@gmail.com"
+git config user.name "Ales Puchilo"
+
+# echo "blog.seangransee.com" > CNAME # domain your site will live at
+git add .
+git commit -m "Deploy from CircleCI" # this will always be the only commit in your gh-pages branch
+
+# use --quiet to avoid printing token in build logs
+git push --force --quiet https://${BLOG_GITHUB_TOKEN}@github.com/savetheales/blog.git master:gh-pages # path to your repo on GitHub, using token for authentication
